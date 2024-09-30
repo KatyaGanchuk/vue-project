291e9476d0991413e414cff44f020295c656cddc
unknown <gan4uk.katya@gmail.com>
2024-09-30 22:06:09 +0300

    homework

---
+++ src/components/PageLayout.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineSlots();
+</script>
+
+<template>
+  <div>
+    <header>
+      <slot name="header"></slot>
+    </header>
+
+    <main>
+      <slot></slot>
+    </main>
+
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+header,
+footer {
+  padding: 10px;
+  background-color: #6c98ff;
+  margin-bottom: 10px;
+}
+
+main {
+  padding: 10px;
+}
+</style>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="user-card">
+    <h3>{{ name }}</h3>
+    <p>Age: {{ age }}</p>
+    <p v-if="age < 18" class="not-allowed">Not Allowed</p>
+  </div>
+</template>
+
+<script setup>
+const { name, age } = defineProps({
+  name: String,
+  age: Number,
+});
+</script>
+
+<style scoped>
+.user-card {
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.not-allowed {
+  color: red;
+  font-weight: bold;
+}
+</style>
