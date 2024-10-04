dc8f30b1e09273d5e2e815c9d81617e8f7aa3465
unknown <gan4uk.katya@gmail.com>
2024-10-04 15:31:36 +0300

    added homework

---
+++ src/components/AgeInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="input-field">
+        <label for="age">Age:</label>
+        <input id="age" :value="modelValue" @input="$emit('update:modelValue', $event.target.valueAsNumber)"
+            type="number" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        modelValue: Number
+    }
+}
+</script>
+
+<style scoped>
+.input-field {
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+}
+input{
+    border-radius: 20px;
+    padding: 5px;
+}
+</style>
---
+++ src/components/NameInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="input-field">
+        <label for="name">Name:</label>
+        <input id="name" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" type="text" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        modelValue: String
+    }
+}
+</script>
+
+<style scoped>
+.input-field {
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+}
+
+input{
+    border-radius: 20px;
+    padding: 5px;
+}
+</style>
